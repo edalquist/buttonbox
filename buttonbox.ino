5bcc5455f0f5c73e11098908c90cd0a1f136eae7
Eric Dalquist <eric.dalquist@gmail.com>
2020-09-24 22:07:11 -0700

    Shortcuts are working

--- buttonbox.ino
+++ buttonbox.ino
@@ -1,17 +1,6 @@
 /*******************************************************************
- *  A simple Macro keyboard built with Arduino Pro Micro and a        
- *  4*4 Button Matrix.
- *  
- *  Parts:
- *  Arduino Pro Micro* - http://s.click.aliexpress.com/e/FQmMd5uc 
- *  4*4 Keypad Button Matrix* - http://s.click.aliexpress.com/e/CqnjTgGg 
- *  
- *  * = Affilate
+ *  A simple Macro keyboard built with Arduino Pro Micro
  *                                                                 
- *  Written by Brian Lough
- *  YouTube: https://www.youtube.com/brianlough
- *  Tindie: https://www.tindie.com/stores/brianlough/
- *  Twitter: https://twitter.com/witnessmenow                                 
  *******************************************************************/
 
 // ----------------------------
@@ -29,22 +18,6 @@
 using namespace ace_button;
 // https://github.com/bxparks/AceButton
 
-/**
- * right side
- *   gnd
- *   rst - n/c
- *   vcc
- *   A2 - re up
- *   A3 - re down
- *   A4 - re button
- * left side
- *   gnd
- *   2 - btn
- *   3 - btn
- *   4 - btn
- *   5 - btn
- *   6 - btn
- */
 
 // Button Pinout
 const int RED_PIN = 4;
@@ -53,7 +26,7 @@ const int GREEN_PIN = 5;
 const int YELLOW_PIN = 2;
 const int BLACK_PIN = 6;
 
-const int ledPin =  17;      // the number of the LED pin (RXLED)
+const int LED_PIN =  17;      // the number of the LED pin (RXLED) on ProMicro
 
 AceButton redButton(RED_PIN);
 AceButton blueButton(BLUE_PIN);
@@ -61,16 +34,15 @@ AceButton greenButton(GREEN_PIN);
 AceButton yellowButton(YELLOW_PIN);
 AceButton blackButton(BLACK_PIN);
 
-SimpleRotary rotary(21, 20, 19);
+SimpleRotary rotary(20, 21, 19);
 
 void handleEvent(AceButton*, uint8_t, uint8_t);
-void sendMacroCommand(uint8_t key);
 
 void setup() {
   Serial.begin(9600);
 
   // initialize the LED pin as an output:
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
 
   // initialize the pushbutton pins as an input:
   pinMode(RED_PIN, INPUT_PULLUP);
@@ -79,81 +51,81 @@ void setup() {
   pinMode(YELLOW_PIN, INPUT_PULLUP);
   pinMode(BLACK_PIN, INPUT_PULLUP);
 
+  // init the event handlers
   redButton.setEventHandler(handleEvent);
   blueButton.setEventHandler(handleEvent);
   greenButton.setEventHandler(handleEvent);
   yellowButton.setEventHandler(handleEvent);
   blackButton.setEventHandler(handleEvent);
-
+  
   // init media key support
   Consumer.begin();
 }
 
 void loop() {
-  
   redButton.check();
   blueButton.check();
   greenButton.check();
   yellowButton.check();
   blackButton.check();
-  
-//  int prevState = buttonState; u
-//  // read the state of the pushbutton value:
-//  buttonState = digitalRead(buttonPin);
-//  if (prevState != buttonState) {
-//    Serial.println(buttonState);
-//  
-//    // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-//    if (buttonState == 1) {
-//      Serial.println("down");
-//      // turn LED on:
-//      digitalWrite(ledPin, LOW);
-////      Keyboard.write('A');
-//      sendMacroCommand('m');
-//      delay(100);
-//      Keyboard.releaseAll(); // this releases the buttons
-//    } else {
-//      Serial.println("up");
-//      // turn LED off:
-//      digitalWrite(ledPin, HIGH);
-//    }
-//  }
 
-  auto i = rotary.rotate();
-  if ( i == 1 ) {
-    Serial.println("CW");
-//     Consumer.write(MEDIA_VOLUME_UP);
-  }
-  if ( i == 2 ) {
-    Serial.println("CCW");
-//     Consumer.write(MEDIA_VOLUME_DOWN);
+  switch (rotary.rotate()) {
+    case 1: {
+      Serial.println("CW - Volume Up");
+      Consumer.write(MEDIA_VOLUME_UP);
+      break;
+    }
+    case 2: {
+      Serial.println("CCW - Volume Down");
+      Consumer.write(MEDIA_VOLUME_DOWN);
+      break;
+    }
   }
 
-  // TODO
-  i = rotary.push();
-  if ( i == 1 ) {
+  if (rotary.push() == 1) {
     Serial.println("Pushed");
+    Consumer.write(MEDIA_VOLUME_DOWN);
   }
 }
 
 void handleEvent(AceButton* button, uint8_t eventType, uint8_t buttonState) {
-  Serial.println(button->getPin());
-  Serial.println(eventType);
-  Serial.println(buttonState);
-  switch (eventType) {
-    case AceButton::kEventPressed:
-      digitalWrite(LED_BUILTIN, LOW);
-      break;
-    case AceButton::kEventReleased:
-      digitalWrite(LED_BUILTIN, HIGH);
-      break;
+  if (eventType == AceButton::kEventPressed) {
+    switch (button->getPin()) {
+      case RED_PIN: {
+        Serial.println("RED: Leave");
+        break;
+      }
+      case BLUE_PIN: {
+        Serial.println("BLUE: Camera");
+        Keyboard.press(KEY_LEFT_CTRL);
+        Keyboard.press(KEY_LEFT_SHIFT);
+        Keyboard.press(KEY_O);
+        delay(100);
+        Keyboard.releaseAll();
+        break;
+      }
+      case GREEN_PIN: {
+        Serial.println("GREEN: Hand");
+        break;
+      }
+      case YELLOW_PIN: {
+        Serial.println("YELLOW: Mute");
+        Keyboard.press(KEY_LEFT_CTRL);
+        Keyboard.press(KEY_LEFT_SHIFT);
+        Keyboard.press(KEY_M);
+        delay(100);
+        Keyboard.releaseAll();
+        break;
+      }
+      case BLACK_PIN: {
+        Serial.println("BLACK: Password");
+        Keyboard.print("00Foo#1234567");
+        break;
+      }
+    }
+  } else {
+    Serial.println(button->getPin());
+    Serial.println(eventType);
+    Serial.println(buttonState);
   }
-}
-
-// This will hold down all the following buttons.
-void sendMacroCommand(uint8_t key) {
-  Keyboard.press(KEY_LEFT_CTRL);
-  Keyboard.press(KEY_LEFT_SHIFT);
-//  Keyboard.press(KEY_LEFT_ALT);
-  Keyboard.press(key);
 }
